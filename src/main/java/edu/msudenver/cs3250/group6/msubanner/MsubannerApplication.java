cf1fb58fed7f8293b25bfb8b955de6b39147940a
Andrew Roberts <clownfragment@gmail.com>
2017-09-06 14:30:09 -0600

    Adding the build from Spring Initializr, (http://start.spring.io/)

---
+++ src/main/java/edu/msudenver/cs3250/group6/msubanner/MsubannerApplication.java
@@ -0,0 +1,12 @@
+package edu.msudenver.cs3250.group6.msubanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsubannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsubannerApplication.class, args);
+	}
+}
