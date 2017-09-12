d4ff7af4b0ca9895ad13e3c9a76b73d8a6147ebd
Jesus Torres <jtorre58@msudenver.edu>
2017-09-12 15:10:09 -0600

    Skeleton for Controller Class

---
+++ src/main/java/edu/msudenver/cs3250/group6/msubanner/UserController.java
@@ -0,0 +1,22 @@
+/**
+ * Basic CRUD operations for user
+ * @author Group 6
+ */
+
+@RestController
+public class UserController{
+
+    public createUser{
+
+    }
+
+    public read{
+
+    }
+    public updateUser{
+
+    }
+    public deleteUser{
+
+    }
+}
