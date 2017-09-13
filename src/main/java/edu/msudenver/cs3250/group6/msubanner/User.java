9fa21563fea87b3e189a94a5ee6f116869038ba6
Bobby Wettig <bobbywettig@gmail.com>
2017-09-12 22:22:50 -0600

    adding user class, index.html, and fixing userController

---
+++ src/main/java/edu/msudenver/cs3250/group6/msubanner/User.java
@@ -0,0 +1,29 @@
+package edu.msudenver.cs3250.group6.msubanner;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
