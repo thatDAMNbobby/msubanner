05ba91d5b5001b896abf90ba39739d90e4c600e2
Jvitatoe <Vitatoe5929@msn.com>
2017-10-03 16:24:37 -0600

    DepartmentRepo and Controller

---
+++ src/main/java/edu/msudenver/cs3250/group6/msubanner/department/DepartmentRepository.java
@@ -0,0 +1,10 @@
+package edu.msudenver.cs3250.group6.msubanner.department;
+
+import org.springframework.data.annotation.Persistent;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+@Persistent
+public interface DepartmentRepository extends CrudRepository<Department, Long>{
+}
