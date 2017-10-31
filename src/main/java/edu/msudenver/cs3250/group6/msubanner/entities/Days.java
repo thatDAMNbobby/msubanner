f17d6658df26171bc53dbb6fb80f7fbd5ac75a19
Andrew Roberts <clownfragment@gmail.com>
2017-10-31 17:27:14 -0600

    completed Days class and Tests

--- src/main/java/edu/msudenver/cs3250/group6/msubanner/entities/Days.java
+++ src/main/java/edu/msudenver/cs3250/group6/msubanner/entities/Days.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Persistent Days Class
@@ -21,23 +22,33 @@ public class Days {
     /**
      * ArrayList of Day enums
      */
-    private ArrayList<Day> dayList = new ArrayList<Day>();
+    private HashMap<Integer,String> dayList = new HashMap<Integer,String>();
 
     /**
      * Valid Day to populate dayList
      */
-    private enum Day {
-        MONDAY("Monday");
+    public enum Day {
+        SUNDAY(1, "Sunday"),
+        MONDAY(2, "Monday" ),
+        TUESDAY(3, "Tuesday"),
+        WEDNESDAY(4, "Wednesday"),
+        THURSDAY(5, "Thursday"),
+        FRIDAY(6, "Friday"),
+        SATURDAY(7, "Saturday");
 
+        private int dayNum;
         private String dayName;
 
-        Day(final String dayName) {
+        /**
+         * Constructor for Day
+         *
+         * @param dayName the name of the day
+         * @param dayNum the number value of the day
+         */
+        Day(int dayNum, final String dayName) {
+            this.dayNum = dayNum;
             this.dayName = dayName;
         }
-    }
-
-    public void addDayToDays() {
-
     }
 
     /**
@@ -56,6 +67,45 @@ public class Days {
      */
     public String getId() { return id; }
 
+    /**
+     * Adds an association of number to day to the dayList HashMap
+     *
+     * @param dayToAdd
+     */
+    public void addToDayList(String dayToAdd) {
+        if(dayToAdd.equals(Day.SUNDAY.dayName)) {
+            dayList.put(Day.SUNDAY.dayNum, Day.SUNDAY.dayName);
+        }
+        else if(dayToAdd.equals(Day.MONDAY.dayName)) {
+            dayList.put(Day.MONDAY.dayNum, Day.MONDAY.dayName);
+        }
+        else if(dayToAdd.equals(Day.TUESDAY.dayName)) {
+            dayList.put(Day.TUESDAY.dayNum, Day.TUESDAY.dayName);
+        }
+        else if(dayToAdd.equals(Day.WEDNESDAY.dayName)) {
+            dayList.put(Day.WEDNESDAY.dayNum, Day.WEDNESDAY.dayName);
+        }
+        else if(dayToAdd.equals(Day.THURSDAY.dayName)) {
+            dayList.put(Day.THURSDAY.dayNum, Day.THURSDAY.dayName);
+        }
+        else if(dayToAdd.equals(Day.FRIDAY.dayName)) {
+            dayList.put(Day.FRIDAY.dayNum, Day.FRIDAY.dayName);
+        }
+        else if(dayToAdd.equals(Day.SATURDAY.dayName)) {
+            dayList.put(Day.SATURDAY.dayNum, Day.SATURDAY.dayName);
+        }
+        else {
+            System.out.println("Console Warning: dayToAdd expected a capitalized day name, ie. Monday, Tuesday, etc");
+        }
+    }
 
+    /**
+     * Returns a HashMap of Day values
+     *
+     * @return HashMap of Day values
+     */
+    public HashMap<Integer,String> getDayList() {
+        return dayList;
+    }
 }
 
