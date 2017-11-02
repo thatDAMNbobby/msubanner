6697c0098ca17cb90fd6e3503c15795a503d11a2
Andrew Roberts <clownfragment@gmail.com>
2017-11-02 10:00:20 -0600

    created test class for Semester

--- src/main/java/edu/msudenver/cs3250/group6/msubanner/entities/Semester.java
+++ src/main/java/edu/msudenver/cs3250/group6/msubanner/entities/Semester.java
@@ -1,4 +1,19 @@
 package edu.msudenver.cs3250.group6.msubanner.entities;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Persistent Semester class.
+ */
 public class Semester {
+
+    /**
+     * Semester id number.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
 }
---
+++ src/test/java/edu/msudenver/cs3250/group6/msubanner/entities/SemesterTest.java
@@ -0,0 +1,7 @@
+package edu.msudenver.cs3250.group6.msubanner.entities;
+
+import static org.junit.Assert.*;
+
+public class SemesterTest {
+
+}
