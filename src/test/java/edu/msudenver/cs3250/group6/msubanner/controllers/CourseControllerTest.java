e5497926ce170545aadc81d32263052dbcd0e811
Andrew Roberts <clownfragment@gmail.com>
2017-12-03 15:23:12 -0700

    added CourseControllerTest.java

---
+++ src/test/java/edu/msudenver/cs3250/group6/msubanner/controllers/CourseControllerTest.java
@@ -0,0 +1,36 @@
+package edu.msudenver.cs3250.group6.msubanner.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CourseControllerTest {
+    @Test
+    public void getAllCourses() throws Exception {
+    }
+
+    @Test
+    public void getCourse() throws Exception {
+    }
+
+    @Test
+    public void addCourse() throws Exception {
+    }
+
+    @Test
+    public void updateCourse() throws Exception {
+    }
+
+    @Test
+    public void deleteCourse() throws Exception {
+    }
+
+    @Test
+    public void editCourse() throws Exception {
+    }
+
+    @Test
+    public void addCourseForm() throws Exception {
+    }
+
+}
