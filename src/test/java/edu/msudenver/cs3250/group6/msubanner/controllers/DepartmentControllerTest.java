408fe2bc9009a86b874d144e2cd8f4ffc8d081e7
Andrew Roberts <clownfragment@gmail.com>
2017-12-03 17:06:43 -0700

    DepartmentControllerTest coverage 100%

--- src/test/java/edu/msudenver/cs3250/group6/msubanner/controllers/DepartmentControllerTest.java
+++ src/test/java/edu/msudenver/cs3250/group6/msubanner/controllers/DepartmentControllerTest.java
@@ -6,12 +6,16 @@ import edu.msudenver.cs3250.group6.msubanner.repositories.CourseRepository;
 import edu.msudenver.cs3250.group6.msubanner.ClassLevel;
 import edu.msudenver.cs3250.group6.msubanner.entities.Course;
 import edu.msudenver.cs3250.group6.msubanner.entities.Department;
+import edu.msudenver.cs3250.group6.msubanner.repositories.DepartmentRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RunWith(SpringRunner.class)
@@ -22,7 +26,7 @@ public class DepartmentControllerTest {
     private DepartmentController controller;
 
     @Autowired
-    CourseRepository departmentRepository;
+    DepartmentRepository departmentRepository;
 
     @Test
     public void testContextLoads() throws Exception {
@@ -30,27 +34,61 @@ public class DepartmentControllerTest {
     }
 
     @Test
-    public void getAllDepartments() throws Exception {
+    public void testGetAllDepartments() throws Exception {
+        ModelAndView ret = controller.getAllDepartments();
+        assertThat("departments".equals(ret));
     }
 
     @Test
-    public void getDepartment() throws Exception {
+    public void testGetDepartmentById() throws Exception {
+        Department  someDepartment = new Department("Marine Mammals");
+        someDepartment.setId("9696");
+        departmentRepository.save(someDepartment);
+
+        ModelAndView mav = controller.getDepartment("9696");
+        assertThat("showdepartment".equals(mav));
     }
 
     @Test
-    public void addDepartment() throws Exception {
+    public void testAddDepartment() throws Exception {
+        Map<String, String> body = new HashMap<>();
+        body.put("9696", "Marine Mammals");
+
+        ModelAndView mav = controller.addDepartment(body);
+
+        assertThat("showdepartment".equals(mav));
     }
 
     @Test
-    public void updateDepartment() throws Exception {
+    public void testUpdateDepartment() throws Exception {
+        Department  someDepartment = new Department("Marine Mammals");
+        someDepartment.setId("9696");
+        departmentRepository.save(someDepartment);
+
+        String someDepartmentName = "Marinated Mammals";
+
+        ModelAndView mav = controller.updateDepartment(someDepartmentName, "9696");
+
+        assertThat(mav.getViewName().equals("showdepartment"));
     }
 
     @Test
-    public void deleteDepartment() throws Exception {
+    public void testDeleteDepartment() throws Exception {
+        Department  someDepartment = new Department("Marine Mammals");
+        someDepartment.setId("9696");
+        departmentRepository.save(someDepartment);
+
+        ModelAndView mav = controller.deleteDepartment("9696");
+        assertThat("department".equals(mav));
     }
 
     @Test
     public void editDepartment() throws Exception {
-    }
+        Department  someDepartment = new Department("Marine Mammals");
+        someDepartment.setId("9696");
+        departmentRepository.save(someDepartment);
 
+        ModelAndView mav = controller.editDepartment("9696");
+        assertThat("department".equals(mav));
+    }
 }
