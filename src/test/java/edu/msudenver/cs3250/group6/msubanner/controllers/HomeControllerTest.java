f9a1534f6bae6db1225c62a18efe0008e38e6737
Andrew Roberts <clownfragment@gmail.com>
2017-12-03 17:37:51 -0700

    HomeController at 100%

--- src/test/java/edu/msudenver/cs3250/group6/msubanner/controllers/HomeControllerTest.java
+++ src/test/java/edu/msudenver/cs3250/group6/msubanner/controllers/HomeControllerTest.java
@@ -1,5 +1,33 @@
 package edu.msudenver.cs3250.group6.msubanner.controllers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import edu.msudenver.cs3250.group6.msubanner.repositories.CourseRepository;
+import edu.msudenver.cs3250.group6.msubanner.ClassLevel;
+import edu.msudenver.cs3250.group6.msubanner.entities.Course;
+import edu.msudenver.cs3250.group6.msubanner.entities.Department;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.servlet.ModelAndView;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class HomeControllerTest {
 
+    @Autowired
+    private HomeController controller;
+
+    @Test
+    public void testContextLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+        ModelAndView ret = controller.index();
+        assertThat("/".equals(ret));
+    }
 }
