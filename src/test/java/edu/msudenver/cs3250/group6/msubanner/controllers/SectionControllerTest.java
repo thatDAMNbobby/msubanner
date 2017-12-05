143e56bc076f1027bce63474fc1647d892b2a8e4
Andrew Roberts <clownfragment@gmail.com>
2017-12-05 16:54:44 -0700

    added SectionControllerTests.java

---
+++ src/test/java/edu/msudenver/cs3250/group6/msubanner/controllers/SectionControllerTest.java
@@ -0,0 +1,44 @@
+package edu.msudenver.cs3250.group6.msubanner.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SectionControllerTest {
+    @Test
+    public void getAllSections() throws Exception {
+    }
+
+    @Test
+    public void getSection() throws Exception {
+    }
+
+    @Test
+    public void addSection() throws Exception {
+    }
+
+    @Test
+    public void updateSection() throws Exception {
+    }
+
+    @Test
+    public void addSectionForm() throws Exception {
+    }
+
+    @Test
+    public void editSection() throws Exception {
+    }
+
+    @Test
+    public void deleteSection() throws Exception {
+    }
+
+    @Test
+    public void sectionsBySemester() throws Exception {
+    }
+
+    @Test
+    public void selectSemester() throws Exception {
+    }
+
+}
