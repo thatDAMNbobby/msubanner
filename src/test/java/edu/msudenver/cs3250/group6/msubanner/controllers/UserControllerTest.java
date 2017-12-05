bf0decb932f62de27f51a43669724eb1db824581
Andrew Roberts <clownfragment@gmail.com>
2017-12-05 14:33:57 -0700

    added UserControllerTest.java

---
+++ src/test/java/edu/msudenver/cs3250/group6/msubanner/controllers/UserControllerTest.java
@@ -0,0 +1,40 @@
+package edu.msudenver.cs3250.group6.msubanner.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserControllerTest {
+    @Test
+    public void getAllUsers() throws Exception {
+    }
+
+    @Test
+    public void getUser() throws Exception {
+    }
+
+    @Test
+    public void addUser() throws Exception {
+    }
+
+    @Test
+    public void updateUser() throws Exception {
+    }
+
+    @Test
+    public void deleteUser() throws Exception {
+    }
+
+    @Test
+    public void deleteUserRedirect() throws Exception {
+    }
+
+    @Test
+    public void addUserForm() throws Exception {
+    }
+
+    @Test
+    public void editUser() throws Exception {
+    }
+
+}
